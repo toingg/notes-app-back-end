0974506c4f5097d712b8df0123841768caead7e4
Fachrizal Ardiansyah <fach.ardiansyah@gmail.com>
2024-05-08 20:35:08 +0700

    First commit-finished

---
+++ readme.md
@@ -0,0 +1,3 @@
+Ini adalah proyek dari dicoding "Membangun back-end untuk notes-app"
+
+http://notesapp-v1.dicodingacademy.com/
---
+++ src/routes.js
@@ -0,0 +1,34 @@
+const {
+  addNoteHandler, getAllNoteHandler, editNoteByIdHandler, getNoteByIdHandler, deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+
+];
+
+module.exports = routes;
